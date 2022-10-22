de1db8ae45d0ceab91a14a8e37496877f45cd959
artem <artkon567890@gmail.com>
2022-10-22 14:31:34 +0300

    Commit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
+import sqlite3
+
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,23 +12,37 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    surname = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
 
     def __repr__(self):
-        return f'users {self.id}'
+        return '<User %r>' % self.id
+# @staticmethod
+# def add_new_user(name, password):
+#     connection = sqlite3.connect(f'data/db/users.db')
+#     cursor = connection.cursor()
+#
+#     cursor.execute(
+#         """CREATE TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY, name STRING, password STRING)""")
+#     connection.commit()
+#
+#     new_user = [name, password]
+#     cursor.execute(f"SELECT * FROM users WHERE name = {name}")
+#
+#     if cursor.fetchone() is not None:
+#         cursor.execute(f"DELETE FROM users WHERE name = {name}")
+#         connection.commit()
+#     cursor.execute("INSERT INTO users(name, password) values(?, ?);", new_user)
+#     connection.commit()
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        db.create_all()
-        print('qw')
-        name = ''
-        surname = ''
+        email = request.form['email']
+        password = request.form['password']
 
-        new_user = User(name=name, surname=surname)
-
+        new_user = User(email=email, password=password)
         try:
             db.session.add(new_user)
             db.session.commit()
@@ -34,8 +50,6 @@ def index():
         except Exception as ex:
             print(ex)
             return "DB ERROR"
-
-
     else:
         return render_template("index.html")
 
